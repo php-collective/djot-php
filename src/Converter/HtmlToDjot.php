6d295626c461b987431d448a1f8804ae3d9a796d
mscherer <dereuromark@web.de>
2026-03-30 10:26:15 +0200

    Fix coding standard: avoid count() in loop condition

--- src/Converter/HtmlToDjot.php
+++ src/Converter/HtmlToDjot.php
@@ -615,12 +615,14 @@ class HtmlToDjot
             $colWidthsAttr = $node->getAttribute('data-djot-col-widths');
             if ($colWidthsAttr !== '') {
                 $colWidths = array_map('intval', explode(',', $colWidthsAttr));
-                foreach ($colWidths as $i => $width) {
+                foreach ($colWidths as $width) {
                     $separator[] = str_repeat('-', max(3, $width));
                 }
                 // Fill remaining columns with default width
-                while (count($separator) < $columnCount) {
+                $separatorCount = count($separator);
+                while ($separatorCount < $columnCount) {
                     $separator[] = '---';
+                    $separatorCount++;
                 }
             } else {
                 $separator = array_fill(0, $columnCount, '---');
